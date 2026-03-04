f85ec3172482a86381ae1b1d7625b4237e6053a9
Sanju nepali <sanju@gmail.com>
2026-03-04 10:56:49 +0530

    fix: fileCleanup require fix, add download debug logs

--- src/utils/fileCleanup.js
+++ src/utils/fileCleanup.js
@@ -1,13 +1,21 @@
 // Change this:
-setInterval(() => {
-  setImmediate(() => {
-    youtubeService.cleanupOldFiles(1).catch(console.error);
-  });
-}, 30 * 60 * 1000);
+setInterval(
+  () => {
+    setImmediate(() => {
+      const youtubeService = require("../services/youtubeService");
+      youtubeService.cleanupOldFiles(1).catch(console.error);
+    });
+  },
+  30 * 60 * 1000,
+);
 
 // To this:
-setInterval(() => {
-  setImmediate(() => {
-    youtubeService.cleanupOldFiles(2).catch(console.error);
-  });
-}, 2 * 60 * 60 * 1000); // every 2 hours
+setInterval(
+  () => {
+    setImmediate(() => {
+      const youtubeService = require("../services/youtubeService");
+      youtubeService.cleanupOldFiles(2).catch(console.error);
+    });
+  },
+  2 * 60 * 60 * 1000,
+); // every 2 hours
